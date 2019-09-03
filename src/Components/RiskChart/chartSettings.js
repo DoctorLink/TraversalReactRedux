fda1bc3e288ff2edc8715bc0b471cffe266171d9
David Gruar <davidgruar@gmail.com>
2019-09-03 15:06:46 +0100

    Change colours

--- src/Components/RiskChart/chartSettings.js
+++ src/Components/RiskChart/chartSettings.js
@@ -5,5 +5,5 @@ export const barHeight = 15;
 export const barLabelWidth = "40%";
 export const barWidth = "57%";
 export const gridlineLabelHeight = 25;
-export const reducedRiskColor = colors.green100;
-export const reduceableRiskColor = colors.orange100;
+export const reducedRiskColor = colors.chartTurquoise;
+export const reduceableRiskColor = colors.chartPink;
