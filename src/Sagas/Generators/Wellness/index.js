5252890a7752bab3348152e292e67eeb3d91d5a9
David Gruar <davidgruar@gmail.com>
2019-09-24 16:45:41 +0100

    Wellness web API and redux infrastructure

---
+++ src/Sagas/Generators/Wellness/index.js
@@ -0,0 +1,15 @@
+import { call, put } from 'redux-saga/effects'
+import * as actions from '../../../Actions'
+
+export default (api) => function* wellness(action) {
+    if (!api.isConfigured) {
+        console.warn("Health risk API is not configured.");
+        return;
+    }
+    try {
+        const wellness = yield call(api.wellness, action.traversalId, action.conclusions);
+        yield put(actions.hraWellnessSet(wellness));
+    } catch (error) {
+        console.error("wellness error", error)
+    }
+}
